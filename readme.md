4b555b15f760a5a31c8f6a0318104795e9089310
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-10-23 02:12:34 +0530

    update with swagar doc

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ### TypeScript NodeJS Server
 
 ### https://ramandeep-singh-1983.medium.com/step-by-step-setting-up-a-node-js-server-in-typescript-c601c066bc57
+
+### Apple Keyboard Emoji (Ctrl+Command+Space Bar😊)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("It works");
+import express from "express";
+import * as dotenv from "dotenv"
+import sitemap from "express-sitemap-html"
+import cors from "cors";
+import * as uuid from "uuid";
+import * as http_status from "http-status";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8082;
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/",(req, res, next)=>{
+    res.status(http_status.OK).jsonp({
+        message:"It's OK"
+    })
+})
+
+app.post("/comments",(req, res, next)=>{
+    const {title,description} = req.body;
+    res.status(http_status.OK).jsonp({
+        payload:{
+            title,description,
+            uuid:`${uuid.v4()}`
+        }
+    })
+})
+
+
+// Configure API DOC
+sitemap.swagger("TODO APP ~ API DOCS",app);
+app.listen(8082,()=>{
+    console.log(`⚡ Server is running under http://localhost:${PORT}`)
+})
