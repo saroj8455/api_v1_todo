cf0a4658ef084f74d5d20d4cc5e5081f89de3f0e
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-10-23 10:55:01 +0530

    Update DB Connection Config

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import sitemap from "express-sitemap-html"
 import cors from "cors";
 import * as uuid from "uuid";
 import * as http_status from "http-status";
+import AppErrorHandler from "../config/error-handeler";
+import  {CONNECT} from "../config/connection"
 
 
 dotenv.config();
@@ -11,8 +13,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8082;
 
+// app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
+
+// DB Config
+CONNECT();
 
 
 app.get("/",(req, res, next)=>{
@@ -34,6 +40,9 @@ app.post("/comments",(req, res, next)=>{
 
 // Configure API DOC
 sitemap.swagger("TODO APP ~ API DOCS",app);
+
+// Config Error Handler
+app.use(AppErrorHandler)
 app.listen(8082,()=>{
     console.log(`⚡ Server is running under http://localhost:${PORT}`)
 })
